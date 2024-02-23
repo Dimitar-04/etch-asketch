e0b8b0b755853f33177a1a7bb3ae00f16aaff1e0
Dimitar Arsov <dimitararsov04@gmail.com>
2024-02-23 18:56:14 +0100

    Finished the function

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let color='black';
-
-
-
+let click=true;
 
 function populate(size)
 {
@@ -31,9 +29,41 @@ function changeSize(input)
     }
 }
 function colorSquare(){
-    this.style.backgroundColor=color;
+    if(click)
+    {
+        if(color=='random')
+        {
+            this.style.backgroundColor=`hsl(${Math.random()*360},100%,50%)`;
+
+         }
+         else{
+        this.style.backgroundColor=color;
+        }
+    }
 }
 function changecolor(choice)
 {
     color=choice;
 }
+
+function resetBoard()
+{
+    let board=document.querySelector('.board');
+    let squares=board.querySelectorAll('div');
+    squares.forEach((div)=> div.style.backgroundColor='white')
+}
+
+document.querySelector('body').addEventListener("click",(e)=>{
+    if(e.target.tagName != "BUTTON")
+    {
+        click=!click;
+        if(click)
+        {
+             document.querySelector('.mode').textContent='Mode: Coloring';
+        }
+        else{
+            document.querySelector('.mode').textContent='Mode: Not coloring';
+
+        }
+    }
+});
